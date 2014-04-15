16bc8f2bf788001fcc71df58d6202ef8df47feb3
IAmRussianMafia <alekseyz15@yahoo.com>
2014-04-14 20:13:01 -0400

    Update Pong.java
    changed the picking players at the "start game" from file menu

--- Pong.java
+++ Pong.java
@@ -2,13 +2,24 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
+/*
+ * ISTE 121 Programming Domain II
+ * Names: Hassan Ndow, Kevin Whetstone, Abdulla Alam, and Aleksey Zurkowski
+ * Team Project: 06
+ * Due: May 17, 2014
+ * Final Project: Pong Game
+ * Description: The pong is a 1 to 4 player that actually play by blocking 
+ *    the ball avoiding the ball pass your line border. 
+ */
 public class Pong extends JFrame implements ActionListener
 {
+   //Attributes
    private JMenuBar jmb;                                       // the menu bar
    private JMenu jmFile, jmHelp;                               // the menus
    private JMenuItem jmiExit, jmiStart, jmiAbout, jmiRule,  
            jmiRestart;     // the menu items
    
+   //Default Constructor
    public Pong()
    {
       //JMenuBar objects
@@ -46,6 +57,64 @@ public class Pong extends JFrame implements ActionListener
       jmiExit.addActionListener(this); 
       jmiAbout.addActionListener(this);
       jmiRule.addActionListener(this);
+   
+   }
+   
+   public void pickingPlayers()
+   {
+      int numPlayers = 0;
+      
+      
+      String[] palyers = new String[] {"Player 1", "Player 2", "Player 3", "Player 4"};
+      
+      String[] choices = {"Player 1", "Player 2", "Player 3", "Player 4"};
+   
+      //Input dialog box for asking for how many players to join before games actually starts.
+      String playerPick = (String)JOptionPane.showInputDialog(null, "How many player do you wish to play with?"
+             , "Welcome to PONG game", JOptionPane.QUESTION_MESSAGE
+             , null, choices, choices[0]);
+   
+      //When player doesnt pick, it will display error.
+      //example would be, cancel and red x button
+      if (playerPick == null)
+      {
+         JOptionPane.showMessageDialog(null, "You didn't picked a player!");
+         System.exit(0);
+      }
+      else //executes when a player is picking number of players.
+      {
+         //Attributes
+         String funfact = null;
+         numPlayers = 0;
+         
+         switch (playerPick)
+         {
+            case "Player 1":
+               System.out.println("Player 1 only to play Pong Game");
+               numPlayers = 1;
+               System.out.println(numPlayers);
+               break;
+            case "Player 2":
+               System.out.println("Player 2 only to play Pong Game");
+               numPlayers = 2;
+               System.out.println(numPlayers);
+               break;
+            case "Player 3":
+               System.out.println("Player 3 only to play Pong Game");
+               numPlayers = 3;
+               System.out.println(numPlayers);
+               break;
+            case "Player 4":
+               System.out.println("Player 4 only to play Pong Game");
+               numPlayers = 4;
+               System.out.println(numPlayers);
+               break;
+         }
+      }
+      
+      //Testing to see if it is working, after choosing player a server allows clients to join the game and starts the game.
+      System.out.println("Last message " + numPlayers);
+      
 
    }
    
@@ -67,10 +136,10 @@ public class Pong extends JFrame implements ActionListener
       }
       if(choice.equals(jmiStart))
       {
+         pickingPlayers();
       }
       if(choice.equals(jmiAbout))
       {
-         
          JOptionPane.showMessageDialog(null,"121 Final Project: Pong" +
             "\n" + "\nDeveloped by:\n\t Hassan Ndow, Kevin Whetstone,\n\t Aleksey Zurkowski, and Abdullah Alam", "Pong", JOptionPane.INFORMATION_MESSAGE);
       }
@@ -80,59 +149,12 @@ public class Pong extends JFrame implements ActionListener
       }
    }     
    
+   //main method
    public static void main (String [] args)
    {
       //creates a Pong object
       Pong p = new Pong();
-      int numPlayers = 0;
-      String[] palyers = new String[] {"Player 1", "Player 2", "Player 3", "Player 4"};
-      
-      String[] choices = {"Player 1", "Player 2"
-                ,"Player 3", "Player 4"};
-
-        //Input dialog box allowing the user to pick a day of the week.
-        String dayPicked = (String)JOptionPane.showInputDialog(null, "How many player do you wish to play with?"
-                , "Welcome to PONG game", JOptionPane.QUESTION_MESSAGE
-                , null, choices, choices[0]);
-
-        //Use an if then else statement to see if the user picked a day
-        //If picked has a null value it means the input dialog box was closed
-        //either by pressing cancel or by using the close 'x' button.
-        if (dayPicked == null)
-        {
-            JOptionPane.showMessageDialog(null, "You didn't picked a player!");
-            System.exit(0);
-        }
-        else //executes when a dayPicked has been a value.
-        {
-            String funfact = "";
-            numPlayers = 0;
-            switch (dayPicked)
-            {
-                case "Player 1":
-                   System.out.println("Player 1 only to play Pong Game");
-                   numPlayers = 1;
-                   System.out.println(numPlayers);
-                    break;
-                case "Player 2":
-                   System.out.println("Player 2 only to play Pong Game");
-                   numPlayers = 2;
-                   System.out.println(numPlayers);
-                    break;
-                case "Player 3":
-                   System.out.println("Player 3 only to play Pong Game");
-                   numPlayers = 3;
-                   System.out.println(numPlayers);
-                    break;
-                case "Player 4":
-                   System.out.println("Player 4 only to play Pong Game");
-                   numPlayers = 4;
-                   System.out.println(numPlayers);
-                    break;
-            }
-        }
-      System.out.println("Last message " + numPlayers);
-      
+       
       
       //Sets the JFrame
       p.setTitle("Pong");
