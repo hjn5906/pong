c82e1ea865878643a3de62793cace69b4773f937
IAmRussianMafia <alekseyz15@yahoo.com>
2014-04-21 13:02:54 -0400

    Update Server.java
    same problem with client

--- Server.java
+++ Server.java
@@ -9,7 +9,8 @@ import java.awt.event.*;
  * To run the server type below (default port set to 1500)
  * > java Server
  * */
-public class Server extends JFrame {
+public class Server extends JFrame 
+   implements ActionListener{
    
    //instance variables
    private Vector<ServerThread> clients;
@@ -18,10 +19,56 @@ public class Server extends JFrame {
    private JTextArea jtaAreaEast, jtaAreaWest;
    private JLabel jlCount, jlWins;
    private JTextField jtCount, jtWins;
+   
+    private JMenuBar jmb;                                       // the menu bar
+   private JMenu jmFile, jmHelp;                               // the menus
+   private JMenuItem jmiExit, jmiStart, jmiAbout, jmiRule,  jmiRestart;     // the menu items
+
+   
+   
    //constructor
    public Server(int port) {
       this.port = port;
       clients = new Vector<ServerThread>();
+      
+      //JMenuBar objects
+      jmb = new JMenuBar(); 
+      jmFile = new JMenu("File");
+      jmHelp = new JMenu("Help");
+      jmiStart = new JMenuItem("Start game");
+      jmiRestart = new JMenuItem("Restart");
+      jmiExit = new JMenuItem("Exit");
+      jmiAbout = new JMenuItem("About");
+      jmiRule = new JMenuItem("Rules");
+      
+      //adding JMenuBar objects to the JFrame
+      jmFile.add(jmiStart);
+      jmFile.add(jmiRestart);
+      jmFile.add(jmiExit); 
+      jmHelp.add(jmiAbout); 
+      jmHelp.add(jmiRule); 
+      jmb.add(jmFile); 
+      jmb.add(jmHelp); 
+      setJMenuBar(jmb);
+      
+      //Mnemonic objects
+      jmFile.setMnemonic(KeyEvent.VK_F);
+      jmHelp.setMnemonic(KeyEvent.VK_H);
+      jmiExit.setMnemonic(KeyEvent.VK_X);
+      jmiAbout.setMnemonic(KeyEvent.VK_A);
+      jmiRule.setMnemonic(KeyEvent.VK_R);
+      jmiStart.setMnemonic(KeyEvent.VK_S);
+      jmiRestart.setMnemonic(KeyEvent.VK_R);
+      
+      // //Adding ActionListener
+      jmiStart.addActionListener(this);
+      jmiRestart.addActionListener(this);
+      jmiExit.addActionListener(this); 
+      jmiAbout.addActionListener(this);
+      jmiRule.addActionListener(this);
+      
+
+      
       
       //WEST BORDER SIDE FOR PONG GAME
       JPanel jpServerWest = new JPanel(new GridLayout(0,1));
@@ -75,7 +122,7 @@ public class Server extends JFrame {
          while(true){
             
             Socket cs = ss.accept();
-            jtaAreaEast.append("Accepting clients at port: " + port);
+            jtaAreaEast.append("Accepting clients at port: " + port + "\n");
             ServerThread st = new ServerThread(cs);
             clients.add(st);
             st.start();
@@ -88,7 +135,93 @@ public class Server extends JFrame {
       
    }
    
+   //ActionListener
+   public void actionPerformed(ActionEvent ae)
+   {
+      Object choice = ae.getSource();
+      
+      if(choice.equals(jmiExit))
+      {
+         System.exit(0);
+      }
+      if(choice.equals(jmiRestart))
+      {
+      }
+      if(choice.equals(jmiStart))
+      {
+         pickingPlayers();
+      }
+      if(choice.equals(jmiAbout))
+      {
+         JOptionPane.showMessageDialog(null,"121 Final Project: Pong" +
+            "\n" + "\nDeveloped by:\n\t Hassan Ndow, Kevin Whetstone,\n\t Aleksey Zurkowski, and Abdullah Alam", "Pong", JOptionPane.INFORMATION_MESSAGE);
+      }
+      if(choice.equals(jmiRule))
+      {
+        
+      }
+   }
    
+   public void pickingPlayers()
+   {
+      int numPlayers = 0;
+      
+      
+      String[] palyers = new String[] {"Player 1", "Player 2", "Player 3", "Player 4"};
+      
+      String[] choices = {"Player 1", "Player 2", "Player 3", "Player 4"};
+   
+      //Input dialog box for asking for how many players to join before games actually starts.
+      String playerPick = (String)JOptionPane.showInputDialog(null, "How many player do you wish to play with?"
+             , "Welcome to PONG game", JOptionPane.QUESTION_MESSAGE
+             , null, choices, choices[0]);
+   
+      //When player doesnt pick, it will display error.
+      //example would be, cancel and red x button
+      if (playerPick == null)
+      {
+         JOptionPane.showMessageDialog(null, "You didn't picked a player!");
+         System.exit(0);
+      }
+      else //executes when a player is picking number of players.
+      {
+         //Attributes
+         String funfact = null;
+         numPlayers = 0;
+         
+         switch (playerPick)
+         {
+            case "Player 1":
+               System.out.println("Player 1 only to play Pong Game");
+               numPlayers = 1;
+               System.out.println(numPlayers);
+               break;
+            case "Player 2":
+               System.out.println("Player 2 only to play Pong Game");
+               numPlayers = 2;
+               System.out.println(numPlayers);
+               break;
+            case "Player 3":
+               System.out.println("Player 3 only to play Pong Game");
+               numPlayers = 3;
+               System.out.println(numPlayers);
+               break;
+            case "Player 4":
+               String name = "Player 4 only to play Pong Game\n";
+               numPlayers = 4;
+               System.out.println(numPlayers);
+               jtaAreaEast.append(name);
+               break;
+         }
+      }
+      
+      //Testing to see if it is working, after choosing player a server allows clients to join the game and starts the game.
+      System.out.println("Last message " + numPlayers);
+      
+
+   }
+   
+   //end ActionListener
    //Server thread
    class ServerThread extends Thread {
    
